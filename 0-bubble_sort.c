1bb49da1bdd56632be98d061f9fdbdc7916e3551
saminstein <saminstein@yahoo.com>
2024-04-27 20:32:04 +0100

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,3 +10,24 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i = 0, j = 0;
+	int temp;
+
+	if (!array || size < 2)
+		return;
+
+	while (i < size)
+	{
+		for (j = 0; j < (size - 1); j++)
+		{
+			if (array[j] >  array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+}
