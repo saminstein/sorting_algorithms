a0918b4200b8a567383638ea1477d6ab64971f8d
Saminstein <saminstein@yahoo.com>
2024-04-27 11:21:51 -0700

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - function that sorts an array of inte
+ * gers
+ * @array: The array of integers
+ * @size: The size of the array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
