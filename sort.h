7527b42af35057075e2902b01ddafae211bbb9e6
Dafinci <odelanadavid@gmail.com>
2024-04-30 21:34:56 -0700

    quickso_rt

--- sort.h
+++ sort.h
@@ -28,10 +28,12 @@ void swap_nodes(listint_t *a, listint_t *b);
 
 /* Prototype for Selection sort */
 void selection_sort(int *array, size_t size);
-void swap_linked_list(listint_t **head, listint_t **first, listint_t *second);
+void swap_linked_list(listint_t **head, listint_t **first, listint_t *second;
 
 /* Prototypes for Quick sort */
 void quick_sort(int *array, size_t size);
 void lomuto_sort(int *array, size_t size, int left, int right);
 
+void quick_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
